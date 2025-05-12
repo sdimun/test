fccbf90058d603896bd226faa649a591f8f66760
sdimun <s.dimun@gmail.com>
2025-05-12 16:25:35 +0200

    Update TC's

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -12,10 +12,36 @@ describe("Login", () => {
   it("Success login and logout", () => {
     cy.login('standard_user','secret_sauce');
 
-    // chceck if header is displayed after login
+    // chceck if eshop screen is displayed
+    cy.get('.bm-burger-button').should('be.visible');
+    cy.get('[data-test="title"]').should("be.visible");
     cy.get(".app_logo").should("be.visible");
+    cy.get('[data-test="shopping-cart-link"]').should("be.visible");
+    cy.get('[data-test="product-sort-container"]').should("be.visible");
 
+    cy.get('[data-test="inventory-item"]').then((list) => {
+      const length = list.length;
+      expect(length).eq(6);
+    });
+
+    cy.get('[data-test="footer"]').within(() => {
+      cy.get('[data-test="social-twitter"]').should('exist');
+      cy.get('[data-test="social-facebook"]').should('exist');
+      cy.get('[data-test="social-linkedin"]').should('exist');
+      cy.get('[data-test="footer-copy"]').should('exist');
+    });
+    
+    // open navBar
     cy.openNavBar();
+    cy.get('.bm-menu').within(()=>{
+      cy.get('[data-test="inventory-sidebar-link"]').should('exist');
+      cy.get('[data-test="about-sidebar-link"]').should('exist');
+      cy.get('[data-test="logout-sidebar-link"]').should('exist');
+      cy.get('[data-test="reset-sidebar-link"]').should('exist');
+    });
+    cy.get('.bm-cross-button').should('be.visible');
+
+
     cy.logoutBtn().click();
 
     cy.loginBtn().should("be.visible");
--- cypress/e2e/orderTest.cy.js
+++ cypress/e2e/orderTest.cy.js
@@ -7,20 +7,18 @@ describe("Order", () => {
   it("Add and remove product from homeScreen", () => {
     // add item to chart
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    cy.get('[data-test="remove-sauce-labs-backpack"]').should("be.visible");
+    cy.get('[data-test="remove-sauce-labs-backpack"]').should('be.visible').should('have.css', 'border-color', 'rgb(226, 35, 26)');
 
     // remove item from chart from homeScreen
     cy.get('[data-test="remove-sauce-labs-backpack"]').click();
-    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').should(
-      "be.visible"
-    );
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').should('be.visible').should('have.css', 'border-color', 'rgb(19, 35, 34)');
 
   });
 
   it("Add product and remove it from chart ", () => {
     // add item to chart
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    cy.get('[data-test="remove-sauce-labs-backpack"]').should("be.visible");
+    cy.get('[data-test="remove-sauce-labs-backpack"]').should('be.visible').should('have.css', 'border-color', 'rgb(226, 35, 26)');
 
     // check if added item is in cart
     cy.cartBtn().click();
@@ -35,16 +33,20 @@ describe("Order", () => {
   it("Full order with 2 items", () => {
     // add 2 items to cart
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    cy.get('[data-test="remove-sauce-labs-backpack"]').should("be.visible");
+    cy.get('[data-test="remove-sauce-labs-backpack"]').should("be.visible").should('have.css', 'border-color', 'rgb(226, 35, 26)');;
 
     cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-    cy.get('[data-test="remove-sauce-labs-bike-light"]').should("be.visible");
+    cy.get('[data-test="remove-sauce-labs-bike-light"]').should("be.visible").should('have.css', 'border-color', 'rgb(226, 35, 26)');;
 
     // go to chart
     cy.cartBtn().click();
 
     // check if user is on cart screen
     cy.get('[data-test="title"]').contains("Your Cart").should("be.visible");
+    cy.get('[data-test="inventory-item"]').then((list) => {
+      const length = list.length;
+      expect(length).eq(2);
+    });
 
     // continue in order by clicking on checkout
     cy.get('[data-test="checkout"]').click();
@@ -64,9 +66,13 @@ describe("Order", () => {
 
     cy.get('[data-test="finish"]').click();
 
-    //check if order has been successful
+    // check if order has been successful
     cy.get('[data-test="back-to-products"]').should("be.visible");
 
     cy.get('[data-test="back-to-products"]').click();
+
+    // check if user is on home screen - check sorting selector exist
+    cy.get('[data-test="product-sort-container"]').should("be.visible");
+
   });
 });
